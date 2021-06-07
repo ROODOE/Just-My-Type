7a2c4f1664aee76c95a7baa126bfcc41bdebf65b
ROODOE <calebsdad102@gmail.com>
2021-06-07 15:20:43 -0500

    last

--- rework.js
+++ rework.js
@@ -1,4 +1,4 @@
-                                        //Variables for HTML elements
+                                        //Variables/HTML elements
 let target = $('#target');
 let promptContainer = $('#prompt-container');
 let yellowBlock = $('#yellow-block');
@@ -70,85 +70,92 @@ targetLetter.html(characterIndexLetter);
 sentence.html(sentenceArrayIndex);
 
 let startNewTimer = new Date();
-//Check for correct keystroke and progress through sentence
+                                        //Check for correct keystroke and progress through sentence
 $(document).keypress(function(e) {
     console.log(startNewTimer);
                                         //Which key you're pressing
     let keyStroke = e.which;
-                                        //If match
-    if(keyStroke === characterIndexNum) {
+
+        if(keyStroke === characterIndexNum) {
                                         //total character count
-        characterSumCount++;
+            characterSumCount++;
                                         //Display check mark
-        let markWahlberg = $('<span></span>').attr({'class':'glyphicon glyphicon-ok', 'aria-hidden':'true' });
-        feedback.append(markWahlberg);
+            let markWahlberg = $('<span></span>').attr({'class':'glyphicon glyphicon-ok', 'aria-hidden':'true' });
+            feedback.append(markWahlberg);
                                         //Move highlighter to next letter
-        yellowBlock.css({left: '+=17.5'}, 0);
+            yellowBlock.css({left: '+=17.5'}, 0);
                                         // Increment letter and its KeyCode
-        characterCounter += 1;
-        characterIndexNum = sentenceArrayIndex.charCodeAt(characterCounter);
-        characterIndexLetter = sentenceArrayIndex[characterCounter];
-        //Display next target letter in the current sentence
-        targetLetter.html(characterIndexLetter);
-        //Just logging some stuff
-        console.log(characterIndexNum);
-        console.log(characterIndexLetter);
-        console.log(characterSumCount);
-        //Checking if current sentence is completed, and if so, starting second sentence.
-        if(characterCounter === sentenceArrayIndex.length) {
-            //Clear feedback and reset highlighter
-            feedback.empty();
-            
-            yellowBlock.css({left: ''}, 0);
-            
-            characterCounter = 0;
-            sentenceCounter++;
-            sentenceArrayIndex = sentenceArray[sentenceCounter];
-            sentenceArrayLength++;
-            sentence.html(sentenceArrayIndex);
+            characterCounter += 1;
             characterIndexNum = sentenceArrayIndex.charCodeAt(characterCounter);
             characterIndexLetter = sentenceArrayIndex[characterCounter];
+                                        //Display next target letter in the current sentence
             targetLetter.html(characterIndexLetter);
+                                        //Just logging some stuff
+            console.log(characterIndexNum);
+            console.log(characterIndexLetter);
+            console.log(characterSumCount);
+                                        //Checking if current sentence is completed, and if so, starting second sentence.
+            if(characterCounter === sentenceArrayIndex.length) {
+                                        //Clear feedback and reset highlighter
+                feedback.empty();
+                
+                yellowBlock.css({left: ''}, 0);
+                
+                characterCounter = 0;
+                sentenceCounter++;
+                sentenceArrayIndex = sentenceArray[sentenceCounter];
+                sentenceArrayLength++;
+                sentence.html(sentenceArrayIndex);
+                characterIndexNum = sentenceArrayIndex.charCodeAt(characterCounter);
+                characterIndexLetter = sentenceArrayIndex[characterCounter];
+                targetLetter.html(characterIndexLetter);
+                console.log(sentenceArrayIndex.length);
+                console.log(sentenceArrayLength);  
+                
+                // sentenceArrayIndex.length - 1
+            } else if(sentenceArrayLength === 8 && characterCounter === sentenceArrayIndex.length - 1) {
+
+                gameOver();
+            }
+            
+        } else if(keyStroke != characterIndexNum) {
+            errorCount += 1;
+            //Display X for wrong character
+            let markWrongberg = $('<span>WRONG!! Get it together, seriously..</span>').attr({'class':'glyphicon glyphicon-remove', 'aria-hidden':'true' });
+            feedback.append(markWrongberg);
+            console.log(characterSumCount);
+            
+        };
+        //If match
+        
+        //Record time and WPM and ask for restart
+        function gameOver() {
+            let elapsedTime = new Date() - startNewTimer;
+            console.log(elapsedTime)
+            let howLongYouTyped = elapsedTime / 60000;
+            console.log(howLongYouTyped)
+            let inMinutes = Math.floor(howLongYouTyped);
+            console.log(inMinutes);
+            let wpm = Math.round(characterSumCount / 5 / howLongYouTyped);
+            console.log(wpm);
+            promptContainer.css('font-family', 'cursive');
+            let tryAgain = promptContainer.text("You type " + wpm + " words per minute. You made " + errorCount + " mistakes. Would you like to try again? If YES, press F5. If NO, then GET OUTTA HEREE!!");
+            let tryAgainButton = $('<input type="submit" id="try-again" value="Try Again!" style="border-radius: 5px;"></input>');
+            promptContainer.append(tryAgainButton);
+            tryAgainButton.click(function(e) {
+                location.reload();
+            });
+            
+    
+        };
+            console.log(characterCounter);
             console.log(sentenceArrayIndex.length);
-            console.log(sentenceArrayLength);  
             
-            // sentenceArrayIndex.length - 1
-        } else if(sentenceArrayLength === 8 && characterCounter === sentenceArrayIndex.length - 1) {
-            gameOver();
-        }
-            
-    } else if(keyStroke != characterIndexNum) {
-        errorCount += 1;
-                                            //Display X for wrong character
-        let markWrongberg = $('<span>WRONG!! Get it together, seriously..</span>').attr({'class':'glyphicon glyphicon-remove', 'aria-hidden':'true' });
-        feedback.append(markWrongberg);
-        console.log(characterSumCount);
-        
-    };
-    
-                                            //Record time and WPM and ask for restart
-    function gameOver() {
-        let elapsedTime = new Date() - startNewTimer;
-        console.log(elapsedTime)
-        let howLongYouTyped = elapsedTime / 60000;
-        console.log(howLongYouTyped)
-        let inMinutes = Math.floor(howLongYouTyped);
-        console.log(inMinutes);
-        let wpm = Math.round(characterSumCount / 5 / howLongYouTyped);
-        console.log(wpm);
-        let tryAgain = promptContainer.text("You type " + wpm + " words per minute. You made " + errorCount + " mistakes. Would you ike to try again? If YES, press F5. If NO, then GET OUTTA HEREE!!");
-        
-        // if(tryAgain == true) {
-        //     location.reload();
-        // }
-    };
-    console.log(characterCounter);
-    console.log(sentenceArrayIndex.length);
-    
+
 });
-
-// end;
-// gameOver();
-
-// let seconds = Math.floor(Date.now() / 1000)
-// promptContainer.html(elapsedTime);
+        
+        // end;
+        // gameOver();
+        
+        // let seconds = Math.floor(Date.now() / 1000)
+        // promptContainer.html(elapsedTime);
